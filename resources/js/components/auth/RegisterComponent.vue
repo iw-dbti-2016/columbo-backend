80476dc8af118e3c76bfb0687b2e48efb3f89d7c
Vik Vanderlinden <vik.vanderlinden@hotmail.com>
2020-07-06 23:44:59 +0200

    Update login ui + start register ui

--- resources/js/components/auth/RegisterComponent.vue
+++ resources/js/components/auth/RegisterComponent.vue
@@ -4,19 +4,20 @@
 			<Logo class="pt-2 pb-4"></Logo>
 			<FormInput label="E-mail address" type="email" v-model="form.email" :fade="true"></FormInput>
 			<FormInput label="Password" type="password" v-model="form.password" :fade="true"></FormInput>
-			<CheckboxInput class="mt-4" title="Remember me" v-model="form.remember" :fade="true"></CheckboxInput>
-			<input class="mt-4 bg-green-600 px-4 py-3 rounded text-white hover:bg-green-700 cursor-pointer" @click.prevent="submit" type="submit" value="Log in">
+			<FormInput label="Repeat password" type="password" v-model="form.password" :fade="true"></FormInput>
+			<a href="#" target="_blank">Our privacy statement</a>
+			<CheckboxInput class="mt-4" title="I read and agree with the privacy statement" v-model="form.remember" :fade="true"></CheckboxInput>
+			<input class="mt-4 bg-green-600 px-4 py-3 rounded text-white hover:bg-green-700 cursor-pointer" @click.prevent="submit" type="submit" value="Register">
 		</form>
 	</div>
 </template>
 
 <script>
-	import { mapActions } from 'vuex'
 	import FormInput from 'Vue/components/forms/FormInput'
 	import CheckboxInput from 'Vue/components/forms/CheckboxInput'
 
 	export default {
-		name: 'login',
+		name: 'register',
 
 		components: {
 			FormInput,
@@ -36,23 +37,18 @@
 		},
 
 		beforeRouteEnter(to, from, next) {
-            next(component => {
-            	component.ready = true;
+			next(component => {
+				component.ready = true;
 
-                component.stopLoading();
-            })
-        },
+				component.stopLoading();
+			})
+		},
 
 		methods: {
-			...mapActions({
-				login: 'auth/login'
-			}),
-
-			async submit() {
-				await this.login(this.form)
-
-				this.$router.replace({name: 'home'});
-			}
+			submit: function() {
+				console.error('Registration not implemented');
+				alert('Registration not implemented');
+			},
 		}
 	}
 </script>
